4222038f5ac9f92100b3beb24c2a29a31d97edb9
Divyanshu Yadav <divyanshu.yadav@hyrecruitai.com>
2024-07-06 15:49:50 +0530

    feat: add ui and post routes

---
+++ src/main/kotlin/example/com/model/Task.kt
@@ -0,0 +1,11 @@
+package example.com.model
+
+enum class Priority {
+    Low, Medium, High, Vital
+}
+
+data class Task(
+    val name: String,
+    val description: String,
+    val priority: Priority
+)
--- src/main/kotlin/example/com/plugins/Templating.kt
+++ src/main/kotlin/example/com/plugins/Templating.kt
@@ -1,6 +1,11 @@
 package example.com.plugins
 
+import example.com.model.Priority
+import example.com.model.Task
+import example.com.model.TaskRepository
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.thymeleaf.Thymeleaf
@@ -16,8 +21,82 @@ fun Application.configureTemplating() {
         })
     }
     routing {
-        get("/html-thymeleaf") {
-            call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
+        route("/tasks") {
+            get {
+                val tasks = TaskRepository.allTasks()
+                call.respond(
+                    ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
+                )
+            }
+            get("/byName") {
+                val name = call.request.queryParameters["taskName"]
+                if (name == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                val task = TaskRepository.taskByName(name)
+                if (task == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+                call.respond(
+                    ThymeleafContent("single-task", mapOf("task" to task))
+                )
+            }
+            get("/byPriority") {
+                val priorityAsText = call.request.queryParameters["priority"]
+                if (priorityAsText == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@get
+                }
+                try {
+                    val priority = Priority.valueOf(priorityAsText)
+                    val tasks = TaskRepository.tasksByPriority(priority)
+
+
+                    if (tasks.isEmpty()) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@get
+                    }
+                    val data = mapOf(
+                        "priority" to priority,
+                        "tasks" to tasks
+                    )
+                    call.respond(ThymeleafContent("tasks-by-priority", data))
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
+            post {
+                val formContent = call.receiveParameters()
+                val params = Triple(
+                    formContent["name"] ?: "",
+                    formContent["description"] ?: "",
+                    formContent["priority"] ?: ""
+                )
+                if (params.toList().any { it.isEmpty() }) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@post
+                }
+                try {
+                    val priority = Priority.valueOf(params.third)
+                    TaskRepository.addTask(
+                        Task(
+                            params.first,
+                            params.second,
+                            priority
+                        )
+                    )
+                    val tasks = TaskRepository.allTasks()
+                    call.respond(
+                        ThymeleafContent("all-tasks", mapOf("tasks" to tasks))
+                    )
+                } catch (ex: IllegalArgumentException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                } catch (ex: IllegalStateException) {
+                    call.respond(HttpStatusCode.BadRequest)
+                }
+            }
         }
     }
 }
